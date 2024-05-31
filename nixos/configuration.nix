a07d236082411f07cc0dbef0bb0bfaeac78374af
Eddie Nguyen <edward_nguyen@brown.edu>
2024-05-31 01:59:27 -0400

    27 current  2024-05-30 22:09:23  23.11.7313.9d29cd266ceb  6.1.91                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,8 +7,8 @@
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     /etc/nixos/hardware-configuration.nix
+
     ./modules/auto.nix
     ./modules/set.nix
     ./modules/pkg.nix
