dd47c4a556fd3cd11a32dd8bc3a2999c0fa71979
Eddie Nguyen <edward_nguyen@brown.edu>
2024-05-30 21:16:24 -0400

    24 current  2024-05-30 21:16:21  23.11.7313.9d29cd266ceb  6.1.91                          *

--- configuration.nix
+++ configuration.nix
@@ -130,8 +130,6 @@
   ];
 
   services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
   security.pam.services.login.fprintAuth = true;
 
   fonts.packages = with pkgs; [
