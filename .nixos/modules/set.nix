a9cc373b0fbda943da88c008f89587d425e3e2f7
Eddie Nguyen <edward_nguyen@brown.edu>
2024-05-31 23:12:26 -0400

    rename .nixos

---
+++ .nixos/modules/set.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Touchpad settings
+  services.xserver.libinput.touchpad = {
+    naturalScrolling = false;
+    accelProfile = "flat";
+  };
+
+  # Adjust brightness
+  boot.kernelParams = [
+    "acpi_backlight=native"
+    "i915.enable_dpcd_backlight=3"
+  ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {fonts = ["FiraMono"];})
+  ];
+
+  fonts.fontconfig.defaultFonts.monospace = ["FiraMono"];
+
+  # Garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-generations +5";
+  };
+
+  # Auto upgrades
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+  };
+}
