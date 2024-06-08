e221a5a6ebfa303bad0e0450d5cf701f720b1d43
Eddie Nguyen <edward_nguyen@brown.edu>
2024-06-08 03:02:59 -0400

    85 current  2024-06-08 03:02:54  24.11.20240605.e8057b6   6.6.32                          *

--- .nixos/home/wayland/hypr/hyprland/start.nix
+++ .nixos/home/wayland/hypr/hyprland/start.nix
@@ -1,15 +1,17 @@
 {pkgs, ...}: {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "swww-daemon"
-      "waybar"
-      "mako"
-      "wl-gammarelay-rs"
-      "hypridle"
+      "~/.local/bin/hyprstart"
+      # "swww-daemon"
+      # "waybar"
+      # "mako"
+      # "wl-gammarelay-rs"
+      # "hypridle"
     ];
-    exec = [
-      "wl-paste --watch cliphist store"
-      "busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500"
-    ];
+
+    # exec = [
+    #   "wl-paste --watch cliphist store"
+    #   "busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500"
+    # ];
   };
 }
