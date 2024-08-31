0c9690e580664eb944c1ee3f8b12f11c41ad22fb
Eddie Nguyen <edward_nguyen@brown.edu>
2024-08-31 02:55:04 -0400

    band aid solution

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    (writeShellScriptBin "cliphist-rofi" (builtins.readFile ./bin/cliphist-rofi))
+    # (writeShellScriptBin "cliphist-rofi" (builtins.readFile ./bin/cliphist-rofi))
     (writeShellScriptBin "my-cron" (builtins.readFile ./bin/my-cron))
     (writeShellScriptBin "powermenu" (builtins.readFile ./bin/powermenu))
     (writeShellScriptBin "rebuild" (builtins.readFile ./bin/rebuild))
--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   imports = [
+    ./gtk.nix
     ./hypr
     ./kitty.nix
     ./mako.nix
