882a7f54ddc6347ee1f5c376811f4c36785d0b5a
Eddie Nguyen <edward_nguyen@brown.edu>
2024-09-12 15:39:42 -0400

    tmux-vim navigator

--- home/terminal/tmux.nix
+++ home/terminal/tmux.nix
@@ -3,39 +3,65 @@
     enable = true;
     clock24 = true;
     extraConfig = ''
-            # colors
+      # colors
       set -ga terminal-overrides ',xterm-256color:Tc'
       set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
       set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
-            # plugins
-            set -g @plugin 'tmux-plugins/tpm'
-            set -g @plugin 'tmux-plugins/tmux-sensible'
-            set -g @plugin 'christoomey/vim-tmux-navigator'
+      # plugins
+      # set -g @plugin 'tmux-plugins/tpm'
+      # set -g @plugin 'tmux-plugins/tmux-sensible'
+      # set -g @plugin 'christoomey/vim-tmux-navigator'
 
-            # mouse support
-            set -g mouse on
+      # mouse support
+      set -g mouse on
 
-            # Start windows and panes at 1
-            set -g base-index 1
-            set -g pane-base-index 1
-            set-window-option -g pane-base-index 1
-            set-option -g renumber-windows on
+      # Start windows and panes at 1
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
 
-            # color variant
-            set -g @rose_pine_variant 'moon'
+      # color variant
+      set -g @rose_pine_variant 'moon'
 
-            # set prefix
-            unbind C-b
-            set -g prefix C-Space
-            bind C-Space send-prefix
+      # set prefix
+      unbind C-b
+      set -g prefix C-Space
+      bind C-Space send-prefix
 
-            # run plugins
-            run '~/.config/tmux/plugins/tpm/tpm'
+      # run plugins
+      # run '~/.config/tmux/plugins/tpm/tpm'
 
-            # opens panes in current directory
-            bind '"' split-window -v -c "#{pane_current_path}"
-            bind '%' split-window -h -c "#{pane_current_path}"
+      # opens panes in current directory
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
     '';
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = vim-tmux-navigator;
+        extraConfig = ''
+          # Smart pane switching with awareness of Vim splits.
+          # See: https://github.com/christoomey/vim-tmux-navigator
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+              | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|\.?n?vim?x?(-wrapped)?)(diff)?$'"
+          bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+          bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+          bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+          bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+          tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+          if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+          if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+          bind-key -T copy-mode-vi 'C-h' select-pane -L
+          bind-key -T copy-mode-vi 'C-j' select-pane -D
+          bind-key -T copy-mode-vi 'C-k' select-pane -U
+          bind-key -T copy-mode-vi 'C-l' select-pane -R
+          bind-key -T copy-mode-vi 'C-\' select-pane -l
+        '';
+      }
+    ];
   };
 }
