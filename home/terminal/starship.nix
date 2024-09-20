9e49f5c9d5bdc7eeb067d22b0eabe83bf06ecdef
Eddie Nguyen <edward_nguyen@brown.edu>
2024-09-20 09:55:06 -0400

    starship config done

--- home/terminal/starship.nix
+++ home/terminal/starship.nix
@@ -2,17 +2,33 @@
   programs.starship = {
     enable = true;
     settings = {
+      format = "$directory$git_branch$git_state$git_status$nix_shell$line_break$character";
+      right_format = "$cmd_duration";
       c.symbol = " ";
+      cmd_duration.format = "[$duration]($style)";
       directory.read_only = " 󰌾 ";
       docker_context.symbol = "󰡨 ";
-      golang.symbol = " ";
+      golang = {
+        format = "[$symbol]($style)";
+        symbol = " ";
+      };
       nix_shell = {
+        format = "via [$symbol$state]($style)";
         unknown_msg = "unknown";
         symbol = "󱄅 ";
       };
-      rust.symbol = "󱘗 ";
-      lua.symbol = "󰢱 ";
-      package.symbol = "󰏗 ";
+      rust = {
+        format = "[$symbol]($style)";
+        symbol = "󱘗 ";
+      };
+      lua = {
+        format = "[$symbol]($style)";
+        symbol = "󰢱 ";
+      };
+      package = {
+        disabled = true;
+        symbol = "󰏗 ";
+      };
     };
   };
 }
