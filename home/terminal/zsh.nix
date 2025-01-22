19740a0b2dfe9d3b326c43dbdbb5280248270701
Eddie Nguyen <edward_nguyen@brown.edu>
2025-01-22 09:28:17 -0500

    zsh alias (for eza)

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -16,6 +16,26 @@
       ];
     };
 
+    shellAliases = {
+      c = "clear";
+      e = "exit";
+      cp = "cp -i";
+      mv = "mv -i";
+      n = "nvim";
+      ls = "eza --color=always --git --no-filesize --no-user --no-time --icons=always --long --no-permissions";
+      la = "eza -lah --git --icons=always --color=always";
+      tree = "eza --tree";
+      rm = "rmtrash";
+      htop = "btop";
+      cat = "bat";
+      less = "bat";
+      diff = "batdiff";
+      man = "batman";
+      grep = "batgrep -i";
+      icat = "kitten icat";
+      s = "kitten ssh";
+    };
+
     initExtra = ''
       # Source plugins
                 source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
